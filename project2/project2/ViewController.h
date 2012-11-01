b987ab50ac837b7927eb9ee6708091ed47b33bf0
Alexander Herrera <alexcherrera@fullsail.edu>
2012-11-01 10:16:37 -0400

    Created new project folder

---
+++ project2/project2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  project2
+//
+//  Created by Alexander Herrera on 11/1/12.
+//  Copyright (c) 2012 Alexander Herrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
